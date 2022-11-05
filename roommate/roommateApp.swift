a58d506c29a538ac867a3f2522490e8c85d9c174
neilauroni <68305760+neilauroni@users.noreply.github.com>
2022-11-05 11:38:44 -0400

    Initial Commit

---
+++ roommate/roommateApp.swift
@@ -0,0 +1,17 @@
+//
+//  roommateApp.swift
+//  roommate
+//
+//  Created by Neil Auroni on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct roommateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
